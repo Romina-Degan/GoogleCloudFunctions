38c10cf01fa5e305f2089415d2d802ee0469b48e
Romina Degan <r.degan.20@abdn.ac.uk>
2024-03-19 09:29:28 +0000

    Changed clormASP to read the values passed from JSON file

--- clormASP.py
+++ clormASP.py
@@ -39,17 +39,6 @@ class Assignment(Predicate):
 # Triggered by a change in a storage bucket
 @functions_framework.cloud_event
 def clormTimetable(cloud_event):
-    # def readBucket(fileName, bucketName):
-    #     storageClient=storage.Client()
-    #     bucketBlob=storageClient.bucket(bucketName)
-    #     blob=bucketBlob.blob(fileName)
-    #     userData=blob.download_as_bytes().decode("utf8")
-    #     userVals = json.loads(userData)
-    #     numberHive=userVals['numMember']
-    #     print(userVals)
-    #     return numberHive
-    
-    # jsonData=json.loads(contentString)
     currentMembers=0
     #Gain basic info on the file that was just added
     data = cloud_event.data
@@ -63,31 +52,16 @@ def clormTimetable(cloud_event):
     storageClient=storage.Client()
     bucketVal=storageClient.bucket(bucket)
     blob=bucketVal.blob(name)
-    userData=blob.download_as_bytes().decode("utf8")
-    print(userData)
-    try:
-        userJson= json.loads(userVals)
-        print(userJson)
-    except e:
-        print("PLEASEEE WHYYYY "+e)
-
-
-    #Load in the contents of the json file
-
+    userData=blob.download_as_bytes().decode()
+    userVals=json.loads(userData)   
+    maxMembers=userVals['numMember']
     
-    for blob in storageClient.list_blobs(bucket,prefix=name):
+    for blob in storageClient.list_blobs(bucket,prefix=userVals['hiveID']):
         currentMembers+=1
     print(currentMembers)
-    
-    
-    # ctrl=clingo.Control(unifier =[User, Task,Assignment])
-    # ctrl.load(ASP_PROGRAM)
-    
-
-    # users=[User(name=userValues['name'],userID=userValues['userID'],avaliable=DayVals(mon=userValues['Mon'],tue=userValues['Tue'],wed=userValues['Wed'],thu=userValues['Thur'],fri=userValues['Fri'],sat=userValues['Sat'],sun=userValues['Sun'])) for userValues in userDetails['userSpecifications']]
-    # tasks=[Task(name=taskValues['taskName'],duration=taskValues['duration'])for taskValues in taskDetails["TaskValues"][0]["TaskDescriptions"]]  
-    # instances=FactBase(users+tasks)
-    # print(instances)
-
-    # ctrl.add_facts(instances)
-    # ctrl.ground([("base", [])])
+    if maxMembers==currentMembers:
+        print("all Members present")
+        
+        
+    else:
+        print("Not all Members are present so timetable wont be made") 
